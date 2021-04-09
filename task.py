41a37967ea0338a208a842b7a7e395d9445e34bb
kDziadkowiec <>
2021-04-09 10:54:49 +0000

    first version

--- task.py
+++ task.py
@@ -1,26 +1,36 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+import random
+
+class Client:
+  def __init__(self, name, money):
+    self.name = name
+    self.money = money
+
+class Bank:
+  def __init__(self, name):
+    self.name = name
+
+  clients_list = []
+
+  def add_client(self, client):
+    client_account_num = random.randint(100000000000, 999999999999)
+    print('add the amount of money you want to store')
+    money_stored = input()
+    if int(money_stored) > client.money:
+      not_enough_money ='client does not have enough money'
+      return print(not_enough_money)
+    else:
+      self.clients_list.append([client.name, client_account_num, money_stored])
+
+
+
+
+if __name__ == '__main__':
+  bank1 = Bank('bank1')
+  print(bank1.name)
+
+  client1 = Client('pawel jaros', 1000)
+  print(client1.name)
+
+  bank1.add_client(client1)
+  print(bank1.clients_list)
